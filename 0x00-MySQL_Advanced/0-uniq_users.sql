b837bd750d54827e2d784546340e2f142da867a1
Bethek-Iyke <talk2bethel017@gmail.com>
2023-04-13 13:53:44 +0100

    sql advanced

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+--Creates a table users
+--id of integer not null, email is string and name is string
+
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
